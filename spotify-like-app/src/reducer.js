50e08db674d7764c216bde313bf9eae7f9fbe7f4
Arnaud Rebel <arebel@SMB.SQLI.COM>
2020-10-29 12:11:22 +0100

    page elements in progress

---
+++ spotify-like-app/src/reducer.js
@@ -0,0 +1,31 @@
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    // REMOVE after finished developping
+    token: "BQBXR_DCIqg-h3Ga42uh_9tocSDwBRTwpbySObIdOGm05gBpxirWt9j0vH-CmQgIzYx878iJa4liRH34_EnyVUYBnLh9R3NmWi1wOwXRwCoLpAVM8rEC3QDI0Z_7E-PQbxra3U_N0MQfCDaohZw1MblfTwjj"
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+    
+    switch (action.type) {
+        case 'SET_USER':
+            return {
+                ...state,
+                user: action.user
+            }
+
+        case 'SET_TOKEN':
+            return {
+                ...state,
+                token: action.token
+            }
+    
+        default:
+            return state;
+    }
+}
+
+export default reducer;
