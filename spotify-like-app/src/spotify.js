4dc89f0b6303fc20e0648233d03c2e6e47c8f715
Mathieu Levi <mathieulevi@gmail.com>
2020-11-02 14:15:27 +0100

    ci: modules structure example

--- spotify-like-app/src/spotify.js
+++ spotify-like-app/src/spotify.js
@@ -1,19 +1,3 @@
-
-
-export const authEndpoint = "https://accounts.spotify.com/authorize";
-
-const redirectUri = "http://localhost:3000";
-
-const clientId = "dd8660aa390c4a719a58d9645945baff";
-
-const scopes = [
-    "user-read-currently-playing",
-    "user-read-recently-played",
-    "user-read-playback-state",
-    "user-top-read",
-    "user-modify-playback-state"
-];
-
 export const getTokenFromUrl = () => {
     return window.location.hash.substring(1).split('&').reduce((initial, item) => {
         let parts = item.split('=');
@@ -23,5 +7,3 @@ export const getTokenFromUrl = () => {
 
     }, {});
 }
-
-export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
